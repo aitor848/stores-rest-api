d7578fc24a4b8b4b0c122d7cf59e7f88b838a230
aitorfernandezsabio <aitorfernandezsabio@AitorF.local>
2021-04-18 21:58:10 +0200

    Last update v2.

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 
+    'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'aitor'
 api = Api(app)
