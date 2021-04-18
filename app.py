0f4ca5fdc99be581b477d4553019be09ac02f584
aitorfernandezsabio <aitorfernandezsabio@AitorF.local>
2021-04-18 23:51:18 +0200

    Added PSQL environment variable and psycopg2.¡3rd..

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 
-    'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'aitor'
 api = Api(app)
@@ -31,6 +30,5 @@ api.add_resource(UserRegister, '/register')
 
 # RUN APP:
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
